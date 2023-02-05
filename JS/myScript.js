1b262a88c413297e0a9fbcc4adf2699fbc76c5f2
Karandeep <bhushaniitdelhi@gmail.com>
2023-02-05 19:16:43 +0530

    first js commit

---
+++ JS/myScript.js
@@ -0,0 +1,8 @@
+        
+//document.write("hello", "+", " i am here");
+//window.alert("hello i am above");
+//console.log("hello i am into the console bar.");
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
